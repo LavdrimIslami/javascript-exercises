2ebae36937ebb31d8c3d17ea9c54380bc70c4bf5
LavdrimIslamiTU <lavdrim.islami280@gmail.com>
2025-05-22 23:42:22 -0400

    Finished a number of stuff

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+    let result = 0;
+    if(x < 0 || y < 0 || !Number.isInteger(x) || !Number.isInteger(y) || Number.isNaN(x) || Number.isNaN(y)){
+        return "ERROR";
+    }
+    else if(x > y){
+        for(let i = y; i <= x; i++){
+            result += i
+        }
+    }
+    else if(x < y){
+        for(let i = x; i<=y; i++){
+            result += i;
+        }
+    }
 
+    return result;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let answer = 0;
+  answer = (x - 32) * (5/9);
+  return Math.round(answer * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let answer2 = 0;
+  answer2 = x * (9/5) + 32;
+  return Math.round(answer2 * 10) / 10;
 };
 
 // Do not edit below this line
